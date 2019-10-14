3bde29434c4ca39930ef22745b247af04e0a92b2
thasna2122 <56201386+thasna2122@users.noreply.github.com>
2019-10-14 20:42:13 +0530

    Update program.c

--- 6.Producer-Consumer Problem/program.c
+++ 6.Producer-Consumer Problem/program.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 #include<stdlib.h>
 int w, mutex = 1, full = 0, empty = 4, x = 0, b[4];
-main()
+main()//driver code
 {
     int n;
     void producer(); //Function Declaration void consumer();
-    int wait(int);
-    int signal(int);
+    int wait(int);//operation on semaphore
+    int signal(int)//operation on semaphore;
     printf("\nProducer-Consumer Problem\n");
     printf("\n1.Produce\n2.Consume\n3.Exit\n");
     do {
@@ -15,12 +15,12 @@ main()
         switch (n)
         {
         case 1: //Produce if((mutex==1)&&(empty!=0))
-            producer();
+            producer();//go to the function defined below
             break;
             else
                 printf("Buffer full\n");
         case 2: //Consume if((mutex==1)&&(full!=0))
-            consumer();
+            consumer();//go to the function defined below
             break;
             else
                 printf("Buffer empty\n");
@@ -31,18 +31,18 @@ main()
     }
     while (1);
 }
-int wait(int s) //wait
+int wait(int s) //wait operation
 {
     return (--s);
 }
-int signal(int s) //signal
+int signal(int s) //signal operation
 {
     return (++s);
 }
 void producer()
 {
     mutex = wait(mutex);
-    full = signal(full);
+    full = signal(full);//one item will be added to the buffer
     empty = wait(empty);
     printf("Enter the value: ");
     scanf("%d", & b[x]);
@@ -54,7 +54,7 @@ void consumer()
 {
     mutex = wait(mutex);
     full = wait(full);
-    empty = signal(empty);
+    empty = signal(empty);//one item consumed from buffer
     printf("Consumer consumes item %d\n", b[x - 1]);
     x--;
     mutex = signal(mutex);
